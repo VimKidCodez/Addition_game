547518aa443d6d991e39df1375d0cc74f8b47dd2
S D Sriram <85500693+ram1234-cell@users.noreply.github.com>
2021-08-20 22:14:26 +0530

    Add files via upload

---
+++ addition_game.py
@@ -0,0 +1,15 @@
+from random import *
+while True:
+    print('Add and type the following numbers')
+    a = randrange(1,10)
+    b = randrange(1,10)
+    print (a )
+    print (b )
+    d = int(input('Answer: '))
+    c = (a+b)
+
+    if (d) == c :
+        print('Correct\n')
+
+    else:
+        print('Incorrect😡\n')
